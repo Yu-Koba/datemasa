8f0ef2a7b5f1de59b3eae1c5b98fb39a4fe9f8bf
Yu-Koba <56417249+Yu-Koba@users.noreply.github.com>
2019-11-28 14:04:48 +0900

    aaaaaa

--- main.ts
+++ main.ts
@@ -1,5 +1,14 @@
+//% weight=70 icon="\uf075" color=#555555 block="コメント"
 namespace comment {
     //% blockId=show_strings block="コメント %v"
     export function noaction(text: string): void {
+    }
+}
+
+//% weight=70 icon="\uf075" color=#555555 block="安藤"
+namespace andy {
+    //% blockId=0 block="ando %v"
+    export function aaa(text: string): string {
+        return "ando"
     }
 }
