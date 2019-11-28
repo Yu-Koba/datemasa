a06cfe01b6e8023f24fcc58aa9692024754b87d4
Yu-Koba <56417249+Yu-Koba@users.noreply.github.com>
2019-11-28 13:10:06 +0900

    test0001

--- main.ts
+++ main.ts
@@ -1,7 +1,5 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+namespace comment {
+    //% blockId=show_strings block="コメント %v"
+    export function noaction(text: string): void {
+    }
+}
